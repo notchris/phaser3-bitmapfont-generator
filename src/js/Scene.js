da2ab3c1979b177594047d27c4bc3befb8793494
d.golovanev <progit64@ya.ru>
2020-05-14 00:17:16 +0300

    Font map PNG will be generated on transparent background with white text color to easier use in phaser and clear tint usage. Added Google Fonts support.

--- src/js/Scene.js
+++ src/js/Scene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import WebFont from 'webfontloader';
 const js2xmlparser = require("js2xmlparser");
 
 export default class Main extends Phaser.Scene {
@@ -12,60 +13,13 @@ export default class Main extends Phaser.Scene {
 
     }
 
-    generateFont() {
+    generateFont(maxWidth, maxHeight, objArray) {
 
         // Generate Font
-        const testString = "Hello World!";
         const fontFamily = document.querySelector('#fontFamily').value;
         const fontSize = parseInt(document.querySelector('#fontSize').value);
-        const fontColor = document.querySelector('#fontColor').value;
-
-        const container = this.add.container(0, 0, []);
-        container.setSize(512, 512);
-        const charArray = [];
-        const charString = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-        for (let i = 0; i < charString.length; i += 1) {
-            charArray.push(charString[i]);
-        }
-        const objArray = [];
-        const widthArray = [];
-        const heightArray = [];
-        charArray.forEach((char) => {
-            const obj = this.add.text(0,0,char, {
-                fontFamily: fontFamily,
-                fontSize: fontSize + 'px',
-                color: fontColor
-            });
-            widthArray.push(obj.width);
-            heightArray.push(obj.height);
-            objArray.push(obj);
-        });
-
-        container.add(objArray);
-
-        let maxWidth = Math.max(...widthArray);
-        let maxHeight = Math.max(...heightArray);
-
-        Phaser.Actions.GridAlign(objArray, {
-            width: 10,
-            height: 12,
-            cellWidth: maxWidth,
-            cellHeight: maxHeight,
-            position: Phaser.Display.Align.TOP_LEFT,
-            x: maxWidth / 2,
-            y: maxHeight / 2
-        });
 
         const charDataArr = [];
-
-        objArray.forEach((obj) => {
-            // Grid Lines
-            /*const cell =  this.add.graphics();
-            cell.lineStyle(1, 0x00ff00, 1);
-            cell.strokeRect(obj.x, obj.y, maxWidth, maxHeight);
-            container.add(cell);*/
-            obj.x = obj.x + (maxWidth/2) - (obj.width/2);
-        });
 
         objArray.forEach((obj) => {
 
@@ -137,7 +91,25 @@ export default class Main extends Phaser.Scene {
             }
         });
         console.log(xml);
+
+        this.dlCanvas(this.game.canvas, document.querySelector('#fontMap'));
     }
+
+    /**
+     * From SO https://stackoverflow.com/questions/12796513/html5-canvas-to-png-file
+     */
+    dlCanvas(canvas, a) {
+        let dt = canvas.toDataURL('image/png');
+        /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+        dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+        /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+        dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition: attachment; filename=Canvas.png');
+
+        // a.download = document.querySelector('#fontFamily').value + '.png';
+        a.href = dt;
+    };
+
 
     /*testFont() {
         this.load.bitmapFont('test', require('../test.png'), require('../test.xml'));
@@ -148,14 +120,94 @@ export default class Main extends Phaser.Scene {
         this.load.start();
     }*/
 
+    loadFont(font) {
+        WebFont.load({
+            google: {
+                families: [font]
+            },
+            fontloading: () => {
+                this.initResize();
+            }
+        });
+
+    }
+
+
+    initResize() {
+
+        const fontFamily = document.querySelector('#fontFamily').value;
+        const fontSize = parseInt(document.querySelector('#fontSize').value);
+        const fontColor = document.querySelector('#fontColor').value;
+
+        const charArray = [];
+        const charString = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+        for (let i = 0; i < charString.length; i += 1) {
+            charArray.push(charString[i]);
+        }
+
+        /**
+         *
+         * @type {Phaser.GameObjects.Text[]}
+         */
+        const objArray = [];
+        const widthArray = [];
+        const heightArray = [];
+        charArray.forEach((char) => {
+            const obj = this.add.text(0,0,char, {
+                fontFamily: fontFamily,
+                fontSize: fontSize + 'px',
+                color: fontColor
+            });
+            widthArray.push(obj.width);
+            heightArray.push(obj.height);
+            objArray.push(obj);
+        });
+
+        let maxWidth = Math.max(...widthArray);
+        let maxHeight = Math.max(...heightArray);
+
+        const gridWidth = 10;
+        const gridHeight = 12;
+
+
+        Phaser.Actions.GridAlign(objArray, {
+            width: gridWidth,
+            height: gridHeight,
+            cellWidth: maxWidth,
+            cellHeight: maxHeight,
+            position: Phaser.Display.Align.TOP_LEFT,
+            x: maxWidth / 2,
+            y: maxHeight / 2
+        });
+
+        objArray.forEach((obj) => {
+            // Grid Lines
+            /*const cell =  this.add.graphics();
+            cell.lineStyle(1, 0x00ff00, 1);
+            cell.strokeRect(obj.x, obj.y, maxWidth, maxHeight);
+            container.add(cell);*/
+            obj.x = obj.x + (maxWidth/2) - (obj.width/2);
+        });
+
+        const width = maxWidth * gridWidth;
+        const height = maxHeight * gridHeight;
+        this.scene.scene.scale.once('resize', (gameSize,
+                                               baseSize,
+                                               displaySize,
+                                               resolution,
+                                               previousWidth,
+                                               previousHeight) => {
+            // this._bg.fillRect(0, 0, gameSize.width, gameSize.height);
+            this.generateFont(maxWidth, maxHeight, objArray);
+        });
+        this.scale.resize(width, height);
+    }
+
     create() {
-        // Stage bg
-        const bg =  this.add.graphics();
-        bg.fillStyle(0xffffff, 1);
-        bg.fillRect(0, 0, 512, 512);
         const btnGenerate = document.querySelector('#generate');
         btnGenerate.addEventListener('click', () => {
-            this.generateFont();
+            const fontFamily = document.querySelector('#fontFamily').value;
+            this.loadFont(fontFamily);
         });
     }
 
